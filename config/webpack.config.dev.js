361ee4d6a3f75821df6ec195a3dc563d62009f14
kageshiron <kageshiron@gmail.com>
2018-02-21 18:15:33 +0900

    Fix web pack config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -188,14 +188,29 @@ module.exports = {
           // This loader doesn't use a "test" so it will catch all modules
           // that fall through the other loaders.
           {
+            test: /\.svg$/,
+            use: [{
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'img/',    // where the fonts will go
+                publicPath: 'static/'       // override the default path
+              }
+            }]
+          },
+          // "file" loader makes sure assets end up in the `build` folder.
+          // When you `import` an asset, you get its filename.
+          // This loader doesn't use a "test" so it will catch all modules
+          // that fall through the other loaders.
+          {
+            loader: require.resolve('file-loader'),
             // Exclude `js` files to keep "css" loader working as it injects
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.svg$/],
-            loader: require.resolve('file-loader'),
+            exclude: [/\.js$/, /\.html$/, /\.json$/],
             options: {
-              name: 'static/img/[name].[hash:8].[ext]',
+              name: 'static/media/[name].[hash:8].[ext]',
             },
           },
         ],
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -226,6 +226,17 @@ module.exports = {
               }
             }]
           },
+          {
+            test: /\.svg$/,
+            use: [{
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'img/',    // where the fonts will go
+                publicPath: 'static/'       // override the default path
+              }
+            }]
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
